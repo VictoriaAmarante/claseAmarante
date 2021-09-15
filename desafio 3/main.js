a1837646e462b04619f853d0c86d10c7bc875d8f
VictoriaAmarante <88126390+VictoriaAmarante@users.noreply.github.com>
2021-09-14 22:14:14 -0300

    Add files via upload
    Código terminado del desafío nº 3

---
+++ desafio 3/main.js
@@ -0,0 +1,11 @@
+let number = parseInt(prompt("Ingrese un numero para obtener su tabla"));
+
+for (let i = 1; i <= 10; i++) {
+
+ let multi = number * i;
+
+ console.log(number + " X " + i + " = " + multi);
+}
+
+console.log("La tabla que seleccionado a es la del" + " " + number);
+
