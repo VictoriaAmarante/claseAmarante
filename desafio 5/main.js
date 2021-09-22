33e7d570c3d415e707137aa14b6996955d155072
VictoriaAmarante <88126390+VictoriaAmarante@users.noreply.github.com>
2021-09-21 21:40:21 -0300

    Add files via upload
    Desafío 5 finalizado.

---
+++ desafio 5/main.js
@@ -0,0 +1,25 @@
+class Pierdras {
+    constructor(articulo, color, precio, cantidad) {
+        this.articulo = articulo.toLowerCase();
+        this.color = color.toLowerCase();
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+    toString() {
+        `Piedras articulo: ${this.articulo}\nColor de Piedras: ${this.color}\nPrecio: ${this.precio}\nCantidad de piedras ${this.cantidad}`
+    }
+    precioTotal() {
+        let sumaTotal = precio * cantidad
+        alert(`El precio final es de ${sumaTotal}`)
+    }
+}
+
+const articulo = prompt('Ingrese el nombre del articulo que desea comprar')
+const color = prompt('Ingrese el tipo de Piedras')
+const precio = parseInt(prompt('Ingrese el precio de piedras en la pagina'))
+const cantidad = parseInt(prompt('Ingrese la cantidad de iedras que desea llevar'))
+
+const zapato1 = new Piedras (articulo, color, precio, cantidad)
+
+console.log(zPiedras1)
+zapato1.precioTotal()
